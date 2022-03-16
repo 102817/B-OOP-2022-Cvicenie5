c20847333ee92c88141dfa71018788bb598b172d
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-16 15:53:26 +0100

    lambda

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -8,28 +8,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Zviera> zveri = new ArrayList<>(List.of(new Pes("Dunčo"), new Macka("Murko")));
-
-        Set<String> auta = new HashSet<>();
+        List<String> auta = new ArrayList<>();
         auta.add("Volvo");
         auta.add("BMW");
         auta.add("VW");
         auta.add("Audi");
-        auta.add("Volvo");
-        auta.add("BMW");
 
-        System.out.println(auta);
-        System.out.println(auta.size());
-
-        if (auta.contains("Volvo")) {
-            auta.remove("Volvo");
-        }
-
-        List<String> auta2 = new ArrayList<>(List.of("Volvo", "BMW"));
-        System.out.println(auta);
-        auta.removeAll(auta2);
-
-        System.out.println(auta);
-        auta.clear();
+        auta.forEach(s -> {
+            if (Objects.equals(s, "BMW")) {
+                System.out.println("Auto ktore nema smerovky");
+            }
+        });
     }
 }
